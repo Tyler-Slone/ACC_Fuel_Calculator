9303c55f40f0ae4b9cba40e391df4a84285ba57e
Tyler-Slone <136870864+Tyler-Slone@users.noreply.github.com>
2023-12-06 22:33:17 -0500

    feat
    - added structure for catching too much fuel for fuel tank capacity
    
    -need to look into why calculations are coming out wrong
    
    -need to handle stint fuel equaling tank capacity

--- fuel_calculator.py
+++ fuel_calculator.py
@@ -3,10 +3,21 @@ import os
 #* Fuel Calculation based on number of pitstops, race length(time or laps), fuel consumption, and laptime
 #* Presents calculation for a Safe(full formation lap) or Reccomended and displays # of laps
 
-# TODO: Equation simplification
-# TODO: Add feature to take fuel tank size into account
-# TODO: Adjust safe fuel calculation
+#TODO: Add feature to take fuel tank size into account(basic)
+#TODO: Have fuel tank size effect number of pitstops
+#TODO: Look over wording
+#TODO: Equation simplification to reduce parenthesis blindness and adjust safe fuel calculation
 
+#! small_fueltank not working correclty didnt take into consideration matching fuel tank size 
+#! and calculations are coming out wrong
+
+#* Global vars
+fuel_tank_size = 0
+pitstops = 0
+laps = 0
+fuel_consumption = 0
+time = 0
+laptime = 0
 
 #* Terminal line seperation
 def line_seperation():
@@ -16,8 +27,14 @@ def line_seperation():
 
 #* Fuel based on number of laps
 def lap_based_fuel():
+    global fuel_tank_size
+    global pitstops
+    global laps
+    global fuel_consumption
+
     while True:
         try:
+            fuel_tank_size = int(input('What is your max fuel capacity: '))
             pitstops = int(input('Number of pitstops(Input 0 if none): '))
             laps = int(input('Number of laps: '))
             fuel_consumption = float(input('Fuel per lap: '))
@@ -25,36 +42,70 @@ def lap_based_fuel():
 
         except:
             print('Please enter a numeric value.')
-        
-    if pitstops.is_integer() and pitstops > 0:
+    
+
+    if (laps * fuel_consumption) > fuel_tank_size:
+        small_fueltank_lap()
+
+    elif pitstops.is_integer() and pitstops > 0:
         lap_total = laps
         safe = math.ceil((((laps * fuel_consumption) + (fuel_consumption * 3)) / (pitstops + 1)))
-        reccomended = math.ceil((((laps * fuel_consumption) + fuel_consumption) / (pitstops + 1)))
+        recommended = math.ceil((((laps * fuel_consumption) + fuel_consumption) / (pitstops + 1)))
 
         line_seperation()
         print(f'Total laps: {lap_total}\n')
         print(f'Safe fuel per stint(Formation lap): {safe} liters\n')
-        print(f'Reccomended fuel: {reccomended} liters')
+        print(f'Recommended fuel: {recommended} liters')
         line_seperation()
-        quit
 
     else:
         lap_total = laps
         safe = math.ceil((laps * fuel_consumption) + (fuel_consumption * 3))
-        reccomended = math.ceil((laps * fuel_consumption) + fuel_consumption)
+        recommended = math.ceil((laps * fuel_consumption) + fuel_consumption)
 
         line_seperation()
         print(f'Total laps: {lap_total}\n')
         print(f'Safe fuel(Full formation lap): {safe} liters\n')
-        print(f'Reccomended fuel: {reccomended} liters')
+        print(f'Recommended fuel: {recommended} liters')
         line_seperation()
-        quit
+
+
+#* Override for not enough fueltank capacity based on laps
+def small_fueltank_lap():
+    global fuel_tank_size
+    global pitstops
+    global laps
+    global fuel_consumption
+
+    print(f'Your fueltank is not big enough for {pitstops} pitstops. Heres how many pitstops and how much fuel you should get for each stint.\n')
+
+    pitstops = math.ceil((fuel_consumption * laps) / fuel_tank_size)
+    lap_total = laps
+    safe = math.ceil((((laps * fuel_consumption) + (fuel_consumption * 3)) / (pitstops + 1)))
+    recommended = math.ceil((((laps * fuel_consumption) + fuel_consumption) / (pitstops + 1)))
+
+    line_seperation()
+    print(f'Total laps: {lap_total}\n')
+    print(f'Number of pitstops: {pitstops}\n')
+    print(f'Safe fuel per stint(Formation lap): {safe} liters\n')
+    print(f'Recommended fuel: {recommended} liters')
+    line_seperation()
+
+
 
 
 #* Fuel based on race time
 def time_based_fuel():
+    global fuel_tank_size
+    global pitstops
+    global laps
+    global fuel_consumption
+    global time
+    global laptime
+
     while True:
         try:
+            fuel_tank_size = int(input('What is your max fuel capacity: '))
             pitstops = int(input('Number of pitstops(Input 0 if none): '))
             time = int(input('Length of race(in minutes): ')) * 60
             fuel_consumption = float(input('Fuel per lap: '))
@@ -64,50 +115,94 @@ def time_based_fuel():
 
         except:
             print('Please enter a numeric value or check you input values correctly')
+
+
+    if ((time / laptime) * fuel_consumption) > fuel_tank_size:
+        small_fueltank_time()
         
-    if pitstops.is_integer() and pitstops > 0:
+    elif pitstops.is_integer() and pitstops > 0:
         lap_total = math.ceil(time / laptime)
         safe = math.ceil(((lap_total * fuel_consumption) + (fuel_consumption * 3) / (pitstops + 1)))
-        reccomended = math.ceil((((lap_total * fuel_consumption) + fuel_consumption)) / (pitstops + 1))
+        recommended = math.ceil((((lap_total * fuel_consumption) + fuel_consumption)) / (pitstops + 1))
 
         line_seperation()
         print(f'Total Laps: {lap_total}\n')
         print(f'Safe fuel per stint(Full formation lap): {safe} liters\n')
-        print(f'Reccomended fuel per stint: {reccomended} liters')
+        print(f'Recommended fuel per stint: {recommended} liters')
         line_seperation()
-        quit
+        
 
     else:
         lap_total = math.ceil(time / laptime)
         safe = math.ceil((lap_total * fuel_consumption) + (fuel_consumption * 3))
-        reccomended = math.ceil((lap_total * fuel_consumption) + fuel_consumption)
+        recommended = math.ceil((lap_total * fuel_consumption) + fuel_consumption)
 
         line_seperation()
         print(f'Total Laps: {lap_total}\n')
         print(f'Safe fuel(Full formation lap): {safe} liters\n')
-        print(f'Reccomended fuel: {reccomended} liters')
+        print(f'Recommended fuel: {recommended} liters')
         line_seperation()
-        quit
+        
+
+#* Override for not enough fueltank capacity based on time
+def small_fueltank_time():
+    global fuel_tank_size
+    global pitstops
+    global laps
+    global fuel_consumption
+    global time
+    global laptime
+
+    print(f'Your fueltank is not big enough for {pitstops} pitstops. Heres how many pitstops and hownmuch fuel you should get for each stint.\n')
+
+    lap_total = math.ceil(time / laptime)
+    pitstops = math.ceil((fuel_consumption * lap_total) / fuel_tank_size)
+    safe = math.ceil((lap_total * fuel_consumption) + (fuel_consumption * 3))
+    recommended = math.ceil((lap_total * fuel_consumption) + fuel_consumption)
+
+    line_seperation()
+    print(f'Total laps: {lap_total}\n')
+    print(f'Number of pitstops: {pitstops}\n')
+    print(f'Safe fuel per stint(Formation lap): {safe} liters\n')
+    print(f'Recommended fuel: {recommended} liters')
+    line_seperation()
+
+
 
 
 #* Master executable
-def main():
-    while True:
-        race_type = str(input('Is this a timed or lap race? ')).lower()
+# def main():
+#     while True:
+#         race_type = str(input('Is this a timed or lap race? ')).lower()
 
-        if race_type == 'timed':
+#         if race_type == 'timed':
+#             time_based_fuel()
+#             break
+#         elif race_type == 'lap':
+#             lap_based_fuel()
+#             break
+#         elif race_type == 'quit':
+#             break
+#         else:
+#             print('Please enter timed, lap, or quit.')
+#         quit
+
+
+#* New master executable
+def new_main():
+    race_type = str(input('Is this a timed or lap race? ')).lower()
+
+    match race_type:
+        case 'timed':
             time_based_fuel()
-            break
-        elif race_type == 'lap':
+        case 'lap':
             lap_based_fuel()
-            break
-        elif race_type == 'quit':
-            break
-        else:
-            print('Please enter timed, lap, or quit.')
-        quit
+        case 'quit':
+            quit
+        case _:
+                print('Please enter timed, lap, or quit.')
 
 
 #* Runs program
 if __name__ == '__main__':
-    main()
+    new_main()
